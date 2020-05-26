18bbf15163c51d1760bd405477244ba8eeb7bf71
kuvanpt2012 <kboylovemam@gmail.com>
2020-05-26 20:54:25 +0700

    setup

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -4,11 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // cach su dung lib retrofit
+        // 1: khoi tao ra retrofit
+
+        //OkHttpClient : Config cac connection
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                                    .readTimeout(30, TimeUnit.SECONDS)
+                                    .connectTimeout(30, TimeUnit.SECONDS)
+                                    .writeTimeout(30, TimeUnit.SECONDS)
+                                    .retryOnConnectionFailure(true)
+                                    .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                                    .build();
+
+        //Gson : parse du lieu ve dang object cua java
+        Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .disableHtmlEscaping()
+                    .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://khoapham.vn/KhoaPhamTraining/json/tien/")
+                            .client(okHttpClient)
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .build();
+
+
+
+
+        //2: dinh nghia cac request call HTTP
+        //3: goi request muon thuc thi
+        //4 : nhan du lieu tu request thong qua phuong thuc call
+
+
     }
 }
